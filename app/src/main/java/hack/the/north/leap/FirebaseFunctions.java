091a0124aabadf3ea5db1aa59fa13cf2d3c9ec59
Alex Foley <foleyfactor98@gmail.com>
2016-09-17 16:19:43 -0400

    Wrote the :fire:base functions
    Let's start talking to Python :smile:

--- app/src/main/java/hack/the/north/leap/FirebaseFunctions.java
+++ app/src/main/java/hack/the/north/leap/FirebaseFunctions.java
@@ -7,8 +7,19 @@ import com.google.firebase.database.FirebaseDatabase;
  * Created by Alex on 2016-09-17.
  */
 public class FirebaseFunctions {
-    public static void setFist(boolean b) {
+
+    //Usage: setFist(b), which sets the fist property in the firebase
+    public void setFist(boolean b) {
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference("").child("fist");
-        ref.setValue("hi ethan :)");
+        ref.setValue(b);
+    }
+
+    //Usage: setDelta(x,y), which sets deltaX and deltaY respectively.
+    public void setDelta(double x, double y) {
+        DatabaseReference refx = FirebaseDatabase.getInstance().getReference("").child("deltaX");
+        DatabaseReference refy = FirebaseDatabase.getInstance().getReference("").child("deltaY");
+
+        refx.setValue(x);
+        refy.setValue(y);
     }
 }
